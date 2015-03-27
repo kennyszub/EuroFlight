9a8b1c68a4d2548354fde40fd9e02e137c2383c2
Ken Szubzda <kszubzda@box.com>
2015-03-27 14:31:24 -0700

    fixed transition

--- EuroFlight/PushTiltTransition.m
+++ EuroFlight/PushTiltTransition.m
@@ -9,6 +9,8 @@
 #import "PushTiltTransition.h"
 
 #define TRANSITION_DURATION 0.55
+#define TILT_ANGLE 50.0f
+#define VIEWS_GAP -10.0f
 
 @interface PushTiltTransition ()
 @property (nonatomic, assign) BOOL isPushTransition;
@@ -38,11 +40,11 @@
     
     CATransform3D rotationTransform = CATransform3DIdentity;
     rotationTransform.m34 = 1.0 / -500;
-    rotationTransform = CATransform3DRotate(rotationTransform, - 40.0f * M_PI / 180.0f, 0, 1, 0.0f);
+    rotationTransform = CATransform3DRotate(rotationTransform, - TILT_ANGLE * M_PI / 180.0f, 0, 1, 0.0f);
     
     CATransform3D reverseRotationTransform = CATransform3DIdentity;
     reverseRotationTransform.m34 = 1.0 / -500;
-    reverseRotationTransform = CATransform3DRotate(reverseRotationTransform, 40.0f * M_PI / 180.0f, 0, 1, 0.0f);
+    reverseRotationTransform = CATransform3DRotate(reverseRotationTransform, TILT_ANGLE * M_PI / 180.0f, 0, 1, 0.0f);
     
     CATransform3D noTransform = CATransform3DIdentity;
     noTransform = CATransform3DRotate(noTransform, 0, 0, 0, 0.0f);
@@ -51,13 +53,13 @@
     
     if (self.isPushTransition) { // is presenting
         [containerView addSubview:toViewController.view];
-        toViewController.view.frame = CGRectMake(toViewController.view.bounds.size.width + 18, 0, toViewController.view.bounds.size.width, toViewController.view.bounds.size.height);
+        toViewController.view.frame = CGRectMake(toViewController.view.bounds.size.width + VIEWS_GAP, 0, toViewController.view.bounds.size.width, toViewController.view.bounds.size.height);
         
         toViewController.view.layer.transform = reverseRotationTransform;
         
         [UIView animateWithDuration:TRANSITION_DURATION animations:^{
             toViewController.view.frame = CGRectMake(0, 0, toViewController.view.bounds.size.width, toViewController.view.bounds.size.height);
-            fromViewController.view.frame = CGRectMake(-fromViewController.view.bounds.size.width - 18, 0, fromViewController.view.bounds.size.width, fromViewController.view.bounds.size.height);
+            fromViewController.view.frame = CGRectMake(-fromViewController.view.bounds.size.width - VIEWS_GAP, 0, fromViewController.view.bounds.size.width, fromViewController.view.bounds.size.height);
             
             fromViewController.view.layer.transform = rotationTransform;
             toViewController.view.layer.transform = noTransform;
@@ -66,13 +68,13 @@
         }];
     } else { // must be dismissing
         [containerView addSubview:toViewController.view];
-        toViewController.view.frame = CGRectMake(- toViewController.view.bounds.size.width - 18, 0, toViewController.view.bounds.size.width, toViewController.view.bounds.size.height);
+        toViewController.view.frame = CGRectMake(- toViewController.view.bounds.size.width - VIEWS_GAP, 0, toViewController.view.bounds.size.width, toViewController.view.bounds.size.height);
         
         toViewController.view.layer.transform = rotationTransform;
         
         [UIView animateWithDuration:TRANSITION_DURATION animations:^{
             toViewController.view.frame = CGRectMake(0, 0, toViewController.view.bounds.size.width, toViewController.view.bounds.size.height);
-            fromViewController.view.frame = CGRectMake(fromViewController.view.bounds.size.width + 18, 0, fromViewController.view.bounds.size.width, fromViewController.view.bounds.size.height);
+            fromViewController.view.frame = CGRectMake(fromViewController.view.bounds.size.width + VIEWS_GAP, 0, fromViewController.view.bounds.size.width, fromViewController.view.bounds.size.height);
             
             fromViewController.view.layer.transform = reverseRotationTransform;
             toViewController.view.layer.transform = noTransform;
