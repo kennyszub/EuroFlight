74dbe41ddb2b81bdcea8e4350fbfd91d4217dafc
Calvin Tuong <ctuong@box.com>
2015-04-05 15:06:43 -0700

    Empty state for favorites list

--- EuroFlight/FavoritesViewController.m
+++ EuroFlight/FavoritesViewController.m
@@ -35,8 +35,7 @@ NSString * const kFavoritesResultCellIdentifier = @"FavoritesResultCell";
 
     self.tableView.rowHeight = UITableViewAutomaticDimension;
 
-    self.cities = [[FavoritesManager sharedInstance] favoritedCities];
-    [self sortCitiesList];
+    [self getCities];
     
     // set up favorites button
     UIImageView *favoritesImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"favorite-off"]];
@@ -46,6 +45,9 @@ NSString * const kFavoritesResultCellIdentifier = @"FavoritesResultCell";
     [favoritesImageView addGestureRecognizer:tapGesture];
     UIBarButtonItem *favoritesButton = [[UIBarButtonItem alloc] initWithCustomView:favoritesImageView];
     self.navigationItem.rightBarButtonItem = favoritesButton;
+
+    // add observer for when cities get favorited
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(cityWasFavorited) name:FavoritedNotification object:nil];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -86,12 +88,37 @@ NSString * const kFavoritesResultCellIdentifier = @"FavoritesResultCell";
 
 #pragma mark - Private methods
 
+- (void)getCities {
+    self.cities = [[FavoritesManager sharedInstance] favoritedCities];
+
+    if (self.cities.count == 0) {
+        UILabel *messageLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height)];
+
+        messageLabel.text = @"You don't have any favorites yet.";
+        messageLabel.textColor = [[UIColor alloc] initWithRed:39/255.0 green:159/255.0 blue:190/255.0 alpha:1.0];
+        messageLabel.numberOfLines = 0;
+        messageLabel.textAlignment = NSTextAlignmentCenter;
+        messageLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:18];
+        [messageLabel sizeToFit];
+
+        self.tableView.backgroundView = messageLabel;
+        self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    } else {
+        [self sortCitiesList];
+    }
+}
+
 - (void)sortCitiesList {
     self.cities = [self.cities sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
         float price1 = ((City *) obj1).lowestCost;
         float price2 = ((City *) obj2).lowestCost;
         return [@(price1) compare:@(price2)];
     }];
+}
+
+- (void)cityWasFavorited {
+    [self getCities];
+    [self.tableView reloadData];
 }
 
 @end
